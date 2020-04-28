4a5f27eeaaece001dd0b268a62cb8385edd05e7c
branhoff <branhoff2@gmail.com>
2020-04-28 01:24:28 -0500

    adding feature for user to navigate directory for broader project applicability

---
+++ user_window.py
@@ -0,0 +1,18 @@
+
+class UserWindow:
+
+    def __init__(self):
+        pass
+
+    def prompt(self):
+        pass
+
+    def file_browser(self):
+        print("File Browser Run")
+
+    def file_name_splitter(self):
+        pass
+
+if __name__ == "__main__":
+    test = UserWindow()
+    test.file_browser()
